ebe1b8f874d98dfe6ee250bee65a2491b5ae27ca
brightstudent <70577787+brightstudent@users.noreply.github.com>
2021-05-20 23:17:10 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,28 @@
-console.log('On')
+console.log("On");
+
+// const allArticles = document.querySelectorAll('article')
+// function handleClick(event){
+//     event.target.classList.add('show')
+//     console.log("aan")
+// }
+
+// //loop over allemaal heen
+// allArticles.forEach(article => {
+//     article.addEventListener('click', handleClick)
+// })
+
+var hoverzone = document.querySelector(".project.seven");
+var bender = document.querySelector(".bender");
+
+hoverzone.addEventListener("mouseover", gaatSlapen);
+hoverzone.addEventListener("mouseleave", wordtWakker);
+
+function gaatSlapen () {
+    bender.classList.add('slapen');
+    console.log("ZZZ")
+}
+
+function wordtWakker () {
+    bender.classList.remove('slapen');
+    console.log("ZZZ")
+}
